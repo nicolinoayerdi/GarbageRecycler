b5458ffe9eafa51f49b1f6407422646aec13ac8a
nayerdi <nicolinoayerdi@gmail.com>
2019-02-10 18:43:55 -0300

    first commit

---
+++ src/main/java/com/example/GarbageRecycler/GarbageRecyclerApplication.java
@@ -0,0 +1,14 @@
+package com.example.GarbageRecycler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageRecyclerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbageRecyclerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/GarbageRecycler/GarbageRecyclerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.GarbageRecycler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GarbageRecyclerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
